cb05edafa02327f03c7c60163d860fc4cfe9c06f
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-02-04 10:31:42 +0000

    Fixed a bunch of linter warnings

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This app will check changes in any PR for open `Todo` style comments and fail a 
 
 ## Development
 
-To start a local server, you can use [https://smee.io/](https://smee.io/) to create a webhook that will forward to your local server.
+To start a local server, you can use [https://smee.io/](https://smee.io/) to create a webhook that will forward to your local server, such as:
 
 ```bash
-smee --url YOUR_DOMAIN --path /event_handler --port 3000
+smee --url https://smee.io/b2p7TRjSjwxQDJ --path /event_handler --port 3000
 ```
 
 Make sure to also set the Webhook URL in the app settings to the same URL.
--- server.rb
+++ server.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'  # Use the Sinatra web framework
 require 'octokit'       # Use the Octokit Ruby library to interact with GitHub's REST API
 require 'dotenv/load'   # Manages environment variables
@@ -11,88 +13,56 @@ require 'net/http'
 require 'uri'
 
 class GHAapp < Sinatra::Application
-
-  # Sets the port that's used when starting the web server.
   set :port, 3000
   set :bind, '0.0.0.0'
 
-  # Expects the private key in PEM format. Converts the newlines.
   PRIVATE_KEY = OpenSSL::PKey::RSA.new(ENV['GITHUB_PRIVATE_KEY'].gsub('\n', "\n"))
-
-  # Your registered app must have a webhook secret.
-  # The secret is used to verify that webhooks are sent by GitHub.
   WEBHOOK_SECRET = ENV['GITHUB_WEBHOOK_SECRET']
-
-  # The GitHub App's identifier (type integer).
   APP_IDENTIFIER = ENV['GITHUB_APP_IDENTIFIER']
 
-  # Turn on Sinatra's verbose logging during development
   configure :development do
     set :logging, Logger::DEBUG
   end
 
-  # Executed before each request to the `/event_handler` route
   before '/event_handler' do
     get_payload_request(request)
     verify_webhook_signature
 
-    # If a repository name is provided in the webhook, validate that
-    # it consists only of latin alphabetic characters, `-`, and `_`.
-    unless @payload['repository'].nil?
-      halt 400 if (@payload['repository']['name'] =~ /[0-9A-Za-z\-\_]+/).nil?
-    end
+    halt 400 unless @payload['repository'].nil? || (@payload['repository']['name'] =~ /[0-9A-Za-z\-_]+/).nil?
 
     authenticate_app
-    # Authenticate the app installation in order to run API operations
     authenticate_installation(@payload)
   end
 
   post '/event_handler' do
-    # Get the event type from the HTTP_X_GITHUB_EVENT header
-    case request.env['HTTP_X_GITHUB_EVENT']
-    when 'check_suite'
-      # A new check_suite has been created. Create a new check run with status queued
-      if @payload['action'] == 'requested' || @payload['action'] == 'rerequested'
+    event_type = request.env['HTTP_X_GITHUB_EVENT']
+
+    if event_type == 'check_suite' && (@payload['action'] == 'requested' || @payload['action'] == 'rerequested')
+      create_check_run
+    end
+
+    if event_type == 'check_run' && @payload['check_run']['app']['id'].to_s == APP_IDENTIFIER
+      if @payload['action'] == 'created'
+        initiate_check_run
+      elsif @payload['action'] == 'rerequested'
         create_check_run
-      end
-      when 'check_run'
-        # Check that the event is being sent to this app
-        if @payload['check_run']['app']['id'].to_s === APP_IDENTIFIER
-          case @payload['action']
-          when 'created'
-            initiate_check_run
-          when 'rerequested'
-            create_check_run
-          # ADD REQUESTED_ACTION METHOD HERE #
-        end
       end
     end
 
-    200 # success status
+    200
   end
 
   helpers do
-    # Create a new check run with status "queued"
     def create_check_run
       @installation_client.create_check_run(
-        # [String, Integer, Hash, Octokit Repository object] A GitHub repository.
         @payload['repository']['full_name'],
-        # [String] The name of your check run.
         'Todo Blocker',
-        # [String] The SHA of the commit to check
-        # The payload structure differs depending on whether a check run or a check suite event occurred.
         @payload['check_run'].nil? ? @payload['check_suite']['head_sha'] : @payload['check_run']['head_sha'],
-        # [Hash] 'Accept' header option, to avoid a warning about the API not being ready for production use.
         accept: 'application/vnd.github+json'
       )
     end
 
-    # Start the CI process
     def initiate_check_run
-      # Once the check run is created, you'll update the status of the check run
-      # to 'in_progress' and run the CI process. When the CI finishes, you'll
-      # update the check run status to 'completed' and add the CI results.
-
       @installation_client.update_check_run(
         @payload['repository']['full_name'],
         @payload['check_run']['id'],
@@ -101,16 +71,13 @@ class GHAapp < Sinatra::Application
       )
 
       full_repo_name = @payload['repository']['full_name']
-      repository = @payload['repository']['name']
       pull_requests = @payload['check_run']['pull_requests']
       pull_number = pull_requests.first['number'] if pull_requests.any?
-  
-      changes = get_pull_request_changes(full_repo_name, repository, pull_number)
-  
-      # Check for TODO style comments in the repository
+
+      changes = get_pull_request_changes(full_repo_name, pull_number)
+
       check_for_todos(changes)
 
-      # Mark the check run as complete!
       @installation_client.update_check_run(
         @payload['repository']['full_name'],
         @payload['check_run']['id'],
@@ -120,31 +87,31 @@ class GHAapp < Sinatra::Application
       )
     end
 
-    def get_pull_request_changes(full_repo_name, repository, pull_number)
+    def get_pull_request_changes(full_repo_name, pull_number)
       uri = URI("https://api.github.com/repos/#{full_repo_name}/pulls/#{pull_number}")
       req = Net::HTTP::Get.new(uri)
       req['Accept'] = 'application/vnd.github.v3.diff'
-      req['Authorization'] = "token #{@installation_token.to_s}"
-    
-      res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      req['Authorization'] = "token #{@installation_token}"
+
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
         http.request(req)
       end
-    
+
       diff = res.body
-    
+
       current_file = ''
       line_number = 0
       changes = []
-    
+
       diff.each_line do |line|
         if line.start_with?('+++')
-          current_file = line[6..-1].strip
+          current_file = line[6..].strip
         elsif line.start_with?('@@')
           line_number = line.split(' ')[2].split(',')[0].to_i - 1
         elsif line.start_with?('+') && !line.start_with?('+++')
-          changes << { file: current_file, line: line_number, text: line[1..-1] }
+          changes << { file: current_file, line: line_number, text: line[1..] }
         end
-    
+
         line_number += 1 unless line.start_with?('-') || line.chomp == '\ No newline at end of file'
       end
 
@@ -154,11 +121,11 @@ class GHAapp < Sinatra::Application
     def check_for_todos(changes)
       todo_changes = []
       in_block_comment = false
-    
+
       changes.each do |change|
         file_type = File.extname(change[:file])
         text = change[:text].strip
-    
+
         if file_type == '.md'
           in_block_comment = true if text.start_with?('<!--')
           in_block_comment = false if text.end_with?('-->')
@@ -169,86 +136,47 @@ class GHAapp < Sinatra::Application
           todo_changes << change if text.downcase.include?('todo') && (in_block_comment || text.start_with?('//'))
         end
       end
-    
+
       logger.debug "todo_changes: #{todo_changes}"
       todo_changes
     end
 
-    # Saves the raw payload and converts the payload to JSON format
     def get_payload_request(request)
-      # request.body is an IO or StringIO object
-      # Rewind in case someone already read it
       request.body.rewind
-      # The raw text of the body is required for webhook signature verification
       @payload_raw = request.body.read
       begin
         @payload = JSON.parse @payload_raw
-      rescue => e
-        fail  'Invalid JSON (#{e}): #{@payload_raw}'
+      rescue JSON::ParserError => e
+        raise "Invalid JSON (#{e}): #{@payload_raw}"
       end
     end
 
-    # Instantiate an Octokit client authenticated as a GitHub App.
-    # GitHub App authentication requires that you construct a
-    # JWT (https://jwt.io/introduction/) signed with the app's private key,
-    # so GitHub can be sure that it came from the app and not altered by
-    # a malicious third party.
     def authenticate_app
       payload = {
-          # The time that this JWT was issued, _i.e._ now.
-          iat: Time.now.to_i,
-
-          # JWT expiration time (10 minute maximum)
-          exp: Time.now.to_i + (5 * 60),
-
-          # Your GitHub App's identifier number
-          iss: APP_IDENTIFIER
+        iat: Time.now.to_i,
+        exp: Time.now.to_i + (7 * 60),
+        iss: APP_IDENTIFIER
       }
-
-      # Cryptographically sign the JWT.
       jwt = JWT.encode(payload, PRIVATE_KEY, 'RS256')
-
-      # Create the Octokit client, using the JWT as the auth token.
-      @app_client ||= Octokit::Client.new(bearer_token: jwt)
+      @authenticate_app ||= Octokit::Client.new(bearer_token: jwt)
     end
 
-    # Instantiate an Octokit client, authenticated as an installation of a
-    # GitHub App, to run API operations.
     def authenticate_installation(payload)
       @installation_id = payload['installation']['id']
-      @installation_token = @app_client.create_app_installation_access_token(@installation_id)[:token]
+      @installation_token = @authenticate_app.create_app_installation_access_token(@installation_id)[:token]
       @installation_client = Octokit::Client.new(bearer_token: @installation_token)
     end
 
-    # Check X-Hub-Signature to confirm that this webhook was generated by
-    # GitHub, and not a malicious third party.
-    #
-    # GitHub uses the WEBHOOK_SECRET, registered to the GitHub App, to
-    # create the hash signature sent in the `X-HUB-Signature` header of each
-    # webhook. This code computes the expected hash signature and compares it to
-    # the signature sent in the `X-HUB-Signature` header. If they don't match,
-    # this request is an attack, and you should reject it. GitHub uses the HMAC
-    # hexdigest to compute the signature. The `X-HUB-Signature` looks something
-    # like this: 'sha1=123456'.
     def verify_webhook_signature
       their_signature_header = request.env['HTTP_X_HUB_SIGNATURE'] || 'sha1='
       method, their_digest = their_signature_header.split('=')
       our_digest = OpenSSL::HMAC.hexdigest(method, WEBHOOK_SECRET, @payload_raw)
       halt 401 unless their_digest == our_digest
 
-      # The X-GITHUB-EVENT header provides the name of the event.
-      # The action value indicates the which action triggered the event.
       logger.debug "---- received event #{request.env['HTTP_X_GITHUB_EVENT']}"
       logger.debug "----    action #{@payload['action']}" unless @payload['action'].nil?
     end
-
   end
 
-  # Finally some logic to let us run this server directly from the command line,
-  # or with Rack. Don't worry too much about this code. But, for the curious:
-  # $0 is the executed file
-  # __FILE__ is the current file
-  # If they are the same—that is, we are running this file directly, call the
-  # Sinatra run method
-  run! if __FILE__ == $0
+  run! if __FILE__ == $PROGRAM_NAME
 end
