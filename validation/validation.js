3a389b98976179ca0b4a03ec72c4781f45ed8366
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-02-10 13:35:35 +0000

    Updated validation files

--- validation/validation.js
+++ validation/validation.js
@@ -1,4 +1,4 @@
-// TODO: Alerts in this file: 4 if no block comments, 9 if standalone, 8 else #37
+// TODO: Alerts in this file: 9 if multiline comments, 1 if standalone_items, 5 else #37
 let a = "todo";
 //TODO: Alert #37
 let b = "todo";
@@ -7,13 +7,13 @@ let c = "todo";
 /* TODO: Alert #37 */ 
 let d = "todo";
 /* TODO: Alert  #37
-let alertThis = "todo"; #37
+let alertThis = "TODO"; #37
 */
 let e = "todo";
 /*
 TODO: Alert #37 */
 let f = "todo";
-let g = "todo"; // TODO: Standalone Alert
+let g = "todo"; // TODO: Alert
 
 
 
