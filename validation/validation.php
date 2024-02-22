1fd1b1415d2d5ce136eab282df04d5443dc1a734
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-02-22 22:46:59 +0000

    Reset validation files

--- validation/validation.php
+++ validation/validation.php
@@ -1,17 +1,17 @@
 <?php
-// TODO: Alerts in this file: 3 if multiline comments, 3 else 
- 
- 
- 
-# TODO 
- 
- 
- 
-/* TODO */ 
- 
- 
- 
-/* This is a 
- TODO 
-*/ 
-?> 
+// TODO: Alerts in this file: 3 if multiline comments, 3 else
+
+
+
+# TODO
+
+
+
+/* TODO */
+
+
+
+/* This is a
+ TODO
+*/
+?>
