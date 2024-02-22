0feb8621583b8e93ce474fcde04067ec4a7d315f
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-02-22 21:44:35 +0000

    Reset config, validation

--- validation/validation.js
+++ validation/validation.js
@@ -1,20 +1,20 @@
-// TODO: Alerts in this file: ? if multiline comments, 1 if standalone_items, 6 else 
-let a = "todo"; 
-//TODO: Alert 
-let b = "todo"; 
+// TODO: Alerts in this file: ? if multiline comments, 1 if standalone_items, 6 else
+let a = "todo";
+//TODO: Alert
+let b = "todo";
 // TODO Alert
-let c = "todo"; 
-/* TODO: Alert */ 
-let d = "todo"; 
-/* TODO: Alert 
-let alertThis = "TODO"; 
-*/ 
-let e = "todo"; 
-/* 
-TODO: Alert */ 
-let f = "todo"; 
-let g = "todo"; // TODO: Alert 
- 
- 
- 
-/* TODO: Alert */ 
+let c = "todo";
+/* TODO: Alert */
+let d = "todo";
+/* TODO: Alert
+let alertThis = "TODO";
+*/
+let e = "todo";
+/*
+TODO: Alert */
+let f = "todo";
+let g = "todo"; // TODO: Alert
+
+
+
+/* TODO: Alert */
